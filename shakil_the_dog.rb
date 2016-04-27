8eace066567dd2541aa901460b5518de229f97e0
blueskywithachanceofflyingpigs <mlee.js@gmail.com>
2016-04-26 17:22:11 -0700

    initial commit

--- shakil_the_dog.rb
+++ shakil_the_dog.rb
@@ -7,8 +7,61 @@
  
 # You'll probably want to write other methods, but this 
 # encapsulates the core dog logic
-def shakil_the_dog
+def shakil_the_dog(dogcommand)
+  case dogcommand
+    when "woof"
+      puts "WOOF WOOF WOOF"
+    when "shakil stop"
+      puts ""
+    when "Shakil STOP!"
+      puts ""
+    when "meow"
+      puts "woof woof woof woof woof"
+    when "/(treat)+/"
+      puts ""
+    when "go away"
+      puts ""
+
+    else 
+      puts "woof"
+  end
 end
  
 # Run our method
-shakil_the_dog
+puts "what do you want to say to Shakil?"
+
+
+
+in_room = true
+while in_room do
+  answer = gets.chomp
+
+  shakil_the_dog(answer)
+
+  in_room = false if answer == "leave room"
+
+# if answer == "woof"
+#   shakil_the_dog("woof")
+#   break
+# elsif answer == "shakil stop"
+#   shakil_the_dog("shakil stop")
+#   break
+# elsif answer == "Shakil STOP!"
+#   shakil_the_dog("Shakil STOP!")
+#   break
+# elsif answer == "meow"
+#   shakil_the_dog("meow")
+#   break
+# elsif answer == "woof woof woof woof woof"
+#   shakil_the_dog("woof woof woof woof woof")
+#   break
+# elsif answer == "go away"
+#   shakil_the_dog("go away")
+#   break
+# elsif answer == "leave room"
+#   in_room == false
+# else
+#   shakil_the_dog("/.*/")
+#   break
+# end
+end
